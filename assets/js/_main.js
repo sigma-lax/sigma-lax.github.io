9376a02dc5c386a89fe0d4418137cd630adeb17c
garam901127 <garam901127@gmail.com>
2015-11-10 14:01:41 +0900

    about_vertical timeline

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -94,3 +94,13 @@ $(document).ready(function(){
     text: "Share with "
   });
 });
+
+
+// about_vertical timeline
+$(window).on('scroll', function(){
+	$timeline_block.each(function(){
+		if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		}
+	});
+});
